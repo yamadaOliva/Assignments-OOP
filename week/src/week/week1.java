1ae80dcd89031a2b3d36b61c00485886610d95b3
Admin <Admin@192.168.1.7>
2022-04-25 20:44:31 +0700

    check

---
+++ week/src/week/week1.java
@@ -0,0 +1,13 @@
+package week;
+import java.util.*;
+import javax.swing.*;
+public class week1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
+		JOptionPane.showMessageDialog(null, "hello");
+		}
+
+}
